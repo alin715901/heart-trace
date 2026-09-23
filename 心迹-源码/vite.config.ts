b3872b7317a81e06b3296b85f9589edd60f24549
alin715901 <2240063185@qq.com>
2026-09-23 15:01:38 +0800

    Fix formatting of base path in vite.config.ts

--- 心迹-源码/vite.config.ts
+++ 心迹-源码/vite.config.ts
@@ -6,7 +6,7 @@ import process from "process"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/heart-trace/'
+  base: '/heart-trace/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
